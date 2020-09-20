c986a5afb98fabf6d6b1dd4cd50c2eaa439de8d9
Ian Chen <ianre657@gmail.com>
2020-09-20 20:23:43 +0800

    [Feature] add custom lib

--- invisible_hand/__init__.py
+++ invisible_hand/__init__.py
@@ -1,5 +1,7 @@
 from .config.github import init_constants as github_init_constants
 from .config.gsheet import init_constants as gsheet_init_constants
+from colorama import init as colorama_init
+colorama_init(autoreset=True)
 
 github_init_constants()
 gsheet_init_constants()
