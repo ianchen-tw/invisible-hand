b8598333292c68a90517664e2af232e8290db3e4
Ian Chen <ianre657@gmail.com>
2020-11-28 01:17:53 +0800

    cleanup

--- invisible_hand/cli.py
+++ invisible_hand/cli.py
@@ -1,14 +1,3 @@
-# import click
-
-
-# from .scripts.add_students import add_students
-# from .scripts.announce_grade import announce_grade
-# from .scripts.crawl_classroom import crawl_classroom
-# from .scripts.event_times import event_times
-# from .scripts.grant_read_access import grant_read_access
-# from .scripts.patch_project import patch_project
-
-
 import typer
 
 from .scripts.add_students import add_students
@@ -30,16 +19,3 @@ app.command("crawl-classroom")(crawl_classroom)
 app.command("announce-grade")(announce_grade)
 # app.command("dev")(dev)
 
-# @click.group(
-#     invoke_without_command=True, context_settings=dict(help_option_names=["-h", "--help"])
-# )
-# @click.pass_context
-# def cli(ctx):
-#     """Toolkits for github courses"""
-#     if ctx.invoked_subcommand is None:
-#         click.echo(ctx.get_help())
-
-
-# cli.add_command(patch_project)
-# cli.add_command(announce_grade)
-# cli.add_command(crawl_classroom)
