ac0be46611f4d3821adff6c9b8573733fbbe5edf
Ian Chen <ianre657@gmail.com>
2021-11-18 15:58:38 +0800

    add __all__

--- hand/cli/cmd/__init__.py
+++ hand/cli/cmd/__init__.py
@@ -5,3 +5,13 @@ from .grant import grant_read_access
 from .patch import patch_project
 from .publish import publish_grade
 from .times import event_times
+
+__all__ = [
+    "add_students",
+    "check_env",
+    "crawl_classroom",
+    "grant_read_access",
+    "patch_project",
+    "publish_grade",
+    "event_times",
+]
